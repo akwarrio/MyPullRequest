eb813ab5248dd59519bf9301b14ebc9d2106b536
Aftab Khan <aftab.khan@paytmbank.com>
2022-07-29 01:52:01 +0530

    Update the token

--- MyPR/MyPR/Service.swift
+++ MyPR/MyPR/Service.swift
@@ -5,16 +5,23 @@
 
 import Foundation
 
+/*
+ Please use your token
+ -owner
+ -repo
+ */
+var mytoken = "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"
+var owner = "akwarrio"
+var repo = "MyPullRequest"
 class Service{
-    
     
     class func fetchProduct(completionHandler: @escaping (_ result: PullRequests?) -> Void) {
         
-        guard let url = URL(string: "https://api.github.com/repos/akwarrio/MyPullRequest/pulls?state=closed") else {
+        guard let url = URL(string: "https://api.github.com/repos/\(owner)/\(repo)/pulls?state=closed") else {
             return
         }
         var urlRequest = URLRequest(url: url)
-        urlRequest.addValue("token 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", forHTTPHeaderField: "Authorization")
+        urlRequest.addValue("token \(mytoken)", forHTTPHeaderField: "Authorization")
         urlRequest.addValue("application/vnd.github+json", forHTTPHeaderField: "Accept")
         let session = URLSession.shared
         let task = session.dataTask(with: urlRequest) { data, resp, error in
